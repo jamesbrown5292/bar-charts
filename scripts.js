5a75194dd0dca5773fe5afe78824be0f88100ce2
James Brown <jamesbrown5292@gmail.com>
2020-08-12 13:55:21 +0000

    stylesheet and grid container class begun

--- scripts.js
+++ scripts.js
@@ -1,16 +1,20 @@
 let optionsObject = {};
 let dataArray = [12, 34, 56, 23, 67, 34, 56];
+let graphContainers = 0;
 
+//Check the document is ready - has no use - more of a jQuery demo for personal exp 
 $(document).ready(function() {
-
-
-
-  let drawBarChart = function(dataArray, optionsObject, element){
-    $('<div id="graphContainer" class="graphContainer"></div>');
-  }
-
-
-
-
-
+  console.log("Document ready")
 });
+
+//Make a containing div for the graph and append it to the page
+//has the dispay grid property and grid template set
+let makeGraphContainer = function(){
+  if (graphContainers < 1) { //only one per page
+    let graphContainer = $('<div id="graphContainer" class="graphContainer"></div>');
+  $('body').prepend(graphContainer);
+  graphContainers = 1;
+  } 
+};
+
+let 
