6646a2d2e3207b30871b61ae007091b00231db73
James Brown <jamesbrown5292@gmail.com>
2020-08-12 13:00:37 +0000

    first commit from attempt 2 of this project

---
+++ scripts.js
@@ -0,0 +1,16 @@
+let optionsObject = {};
+let dataArray = [12, 34, 56, 23, 67, 34, 56];
+
+$(document).ready(function() {
+
+
+
+  let drawBarChart = function(dataArray, optionsObject, element){
+    $('<div id="graphContainer" class="graphContainer"></div>');
+  }
+
+
+
+
+
+});
